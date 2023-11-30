eefea9f1fed07581a463137445588e461b56d541
Steven McManus <steven.mcmanus@companycam.com>
2023-11-30 13:03:37 -0700

    feat: add memory usage for iOS in swift

--- ios/CompanycamVibeCheck.swift
+++ ios/CompanycamVibeCheck.swift
@@ -12,8 +12,39 @@ class CompanycamVibeCheck: NSObject {
     resolve(state)
   }
 
+  func mach_task_self() -> task_t {
+    return mach_task_self_
+  }
+
+  func getMemoryUsedInBytesUsed() -> Float? {
+    var info = mach_task_basic_info()
+    var count = mach_msg_type_number_t(MemoryLayout.size(ofValue: info) / MemoryLayout<integer_t>.size)
+    let kerr = withUnsafeMutablePointer(to: &info) { infoPtr in
+        return infoPtr.withMemoryRebound(to: integer_t.self, capacity: Int(count)) { (machPtr: UnsafeMutablePointer<integer_t>) in
+            return task_info(
+                mach_task_self(),
+                task_flavor_t(MACH_TASK_BASIC_INFO),
+                machPtr,
+                &count
+            )
+        }
+    }
+    guard kerr == KERN_SUCCESS else {
+        return nil
+    }  
+    return Float(info.resident_size)
+  }
+
   @objc(getMemoryInfo: rejecter:)
   func getMemoryInfo(resolve:RCTPromiseResolveBlock, reject:RCTPromiseRejectBlock) -> Void {
-    resolve(null);
+    let unformattedMemoryUsed = getMemoryUsedInBytesUsed()
+    let formattedMemoryUsed: String
+    if (unformattedMemoryUsed != nil) {
+        formattedMemoryUsed = String(format: "%f", unformattedMemoryUsed!)
+    } else {
+        formattedMemoryUsed = "unable to get memory value"
+    }
+    resolve(formattedMemoryUsed);
+
   }
 }
