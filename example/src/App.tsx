8884c4c4f343f44c87d5b9c5fc45e312d243adeb
Steven McManus <steven.mcmanus@companycam.com>
2023-12-01 14:28:39 -0700

    chore: ignore library for typechecking for now

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,7 +1,11 @@
 import * as React from 'react';
 
 import { StyleSheet, View, Text } from 'react-native';
+// es-lint disable-next-line
+// @ts-ignore
 import { getCurrentVibe, type FullVibeCheckType } from '@companycam/vibe-check';
+// "@react-native-community/netinfo": "^11.1.1",
+// "react-native-device-info": "^10.12.0"
 export default function App() {
   const [info, setInfo] = React.useState<FullVibeCheckType>();
 
